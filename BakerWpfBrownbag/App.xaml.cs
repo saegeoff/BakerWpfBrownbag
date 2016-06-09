31358004581fcb7a8d16b64cc6a71cd7108df2b5
Geoffrey Trees <gptrees@gmail.com>
2016-06-09 13:49:07 -0400

    First lecture

--- BakerWpfBrownbag/App.xaml.cs
+++ BakerWpfBrownbag/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BakerWpfBrownbag.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace BakerWpfBrownbag
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindowView vw = new MainWindowView();
+            // MainWindowViewModel vm = new MainWindowViewModel();
+            // vw.DataContext = vm;
+            vw.Show();
+        }
     }
 }
