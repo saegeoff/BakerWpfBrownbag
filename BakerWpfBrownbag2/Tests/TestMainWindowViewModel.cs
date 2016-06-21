5cf558f63d8e2c4523636d02c01bdfa851d4e6d3
Geoffrey Trees <gptrees@gmail.com>
2016-06-21 13:03:55 -0400

    Brownbag 2

---
+++ BakerWpfBrownbag2/Tests/TestMainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BakerWpfBrownbag.Models;
+using BakerWpfBrownbag.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakerWpfBrownbag.Tests
+{
+    class TestMainWindowViewModel
+    {
+        public void Test()
+        {
+            MainWindowViewModel vm = new MainWindowViewModel(new MatlConcrete());
+
+            
+        }
+    }
+}
